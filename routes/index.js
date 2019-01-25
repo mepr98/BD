b767c35e80c896ead2bf89236e2bb18b5ef79df6
Manuel Prieto Rivas <mepr98>
2019-01-24 23:07:33 -0400

    hhgfj

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const dbconnection = require('../config/database');
 
 router.get("/", (req, res) => {
-  res.render("layout", { title: "Layout" });
+ 
+    res.render("layout", { title: "Layout" });
+  
+  
 });
 
 module.exports = router;
--- start.js
+++ start.js
@@ -7,7 +7,7 @@ sequelize
     console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
   });
 const app = require("./app");
-app.set("port", process.env.PORT || 7777);
+app.set("port", process.env.PORT ||3000);
 const server = app.listen(app.get("port"), () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
